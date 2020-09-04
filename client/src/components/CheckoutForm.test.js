1a947d56f32a96dd7e091da4f1292db7551e9dde
joel vega <joelvega97@yahoo.com>
2020-09-04 12:29:14 -0400

    Was able to capture all the forms in test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByLabelText, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+    render(<CheckoutForm />)
+
+    screen.findByRole('textbox', {
+        name: /first name:/i
+      })
+
+    screen.findByRole('textbox', {
+        name: /last name:/i
+    })
+
+    screen.findByRole('textbox', {
+        name: /address:/i
+    })
+
+    screen.findByRole('textbox', {
+        name: /city:/i
+    })
+
+    screen.findByRole('textbox', {
+        name: /state:/i
+    })
+
+    screen.findByRole('textbox', {
+        name: /zip:/i
+    })
+
+
+});
